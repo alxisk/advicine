1e31ad66a38142a041a50f35d22c21e34faff688
alxisk <alexander.iskovskikh@gmail.com>
2017-06-23 19:18:36 +0300

    add handleSubmit to Search component

--- src/components/search.js
+++ src/components/search.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import PropTypes from 'prop-types';
+import { fetchSearchResults } from '../actions/searchActions';
 
-const Search = () => (
-  <div className="search">
-    <div className="wrap search__wrap">
-      <input type="text" className="search__input" placeholder="Search.." />
-    </div>
-  </div>
-);
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-export default Search;
+  handleSubmit(event) {
+    event.preventDefault();
+    const query = this.searchInput.value;
+    if (!query) return;
+    this.props.onSubmit(query);
+  }
+
+  render() {
+    return (
+      <div className="search">
+        <div className="wrap search__wrap">
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              ref={(input) => { this.searchInput = input; }}
+              className="search__input"
+              placeholder="Search.."
+            />
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onSubmit: bindActionCreators(fetchSearchResults, dispatch)
+  };
+}
+
+Search.propTypes = {
+  onSubmit: PropTypes.func.isRequired
+}
+
+export default connect(null, mapDispatchToProps)(Search);
