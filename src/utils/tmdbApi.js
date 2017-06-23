4e4ead0fe70fb0122c802a7b4f5057b65f741ec5
alxisk <alexander.iskovskikh@gmail.com>
2017-06-23 19:05:03 +0300

    add tmdb api utils

---
+++ src/utils/tmdbApi.js
@@ -0,0 +1,15 @@
+const tmdbApi = {
+  key: '5d974471b2e6b4bf9f03e0c32040eeb',
+
+  getTitles(query) {
+    query = encodeURIComponent(query);
+    // query = 'fight club'; //*delete
+    console.log('tmdbApi.getTitles()');
+    return fetch(`https://api.themoviedb.org/3/search/movie?api_key=${this.key}&query=${query}`)
+      .then(response => response.json(),
+            error => console.log(error))
+      .then(json => json.results);
+  }
+};
+
+export default tmdbApi;
