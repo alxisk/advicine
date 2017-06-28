6adebb84d5fa3f1be0d9efb6cec8e2d5b07cc794
alxisk <alexander.iskovskikh@gmail.com>
2017-06-28 21:55:17 +0300

    add titles actions

---
+++ src/actions/titlesActions.js
@@ -0,0 +1,28 @@
+import * as actionTypes from '../constants/actionTypes';
+import tmdbApi from '../utils/tmdbApi';
+
+export function requestTitleData(id) {
+  return {
+    type: actionTypes.REQUEST_TITLE_DATA,
+    id
+  };
+}
+
+export function recieveTitleData(id, details, videos) {
+  return {
+    type: actionTypes.RECIEVE_TITLE_DATA,
+    id,
+    details,
+    videos
+  };
+}
+
+export const fetchTitleData = titleId => (dispatch) => {
+  dispatch(requestTitleData(titleId));
+  const details = tmdbApi.getDetails(titleId);
+  const videos = tmdbApi.getVideos(titleId);
+  return Promise.all([details, videos])
+    .then(
+      responses => dispatch(recieveTitleData(titleId, ...responses))
+    );
+};
--- src/utils/tmdbApi.js
+++ src/utils/tmdbApi.js
@@ -1,14 +1,28 @@
 const tmdbApi = {
+  baseUrl: 'https://api.themoviedb.org/3/',
   key: '5d974471b2e6b4bf9f03e0c32040eebc',
 
   getTitles(query) {
     query = encodeURIComponent(query);
     // query = 'fight club'; //*delete
     console.log('tmdbApi.getTitles()');
-    return fetch(`https://api.themoviedb.org/3/search/movie?api_key=${this.key}&query=${query}`)
+    return fetch(`${this.baseUrl}search/movie?api_key=${this.key}&query=${query}`)
       .then(response => response.json(),
             error => console.log(error))
       .then(json => json.results);
+  },
+
+  getDetails(titleId) {
+    return fetch(`${this.baseUrl}movie/${titleId}?api_key=${this.key}&language=en-US`)
+      .then(response => response.json(),
+            error => console.log(error));
+  },
+
+  getVideos(titleId) {
+    return fetch(`${this.baseUrl}movie/${titleId}/videos?api_key=${this.key}&language=en-US`)
+      .then(response => response.json(),
+            error => console.log(error))
+      .then(response => response.results);
   }
 };
 
